c5967ca24b0ac87849f83925ad29d7615cec25a5
HyesonAnn <side111229@gmail.com>
2024-06-17 15:24:30 +0900

    init

---
+++ src/main/java/com/multi/gamegather/GamegatherApplication.java
@@ -0,0 +1,13 @@
+package com.multi.gamegather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamegatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamegatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multi/gamegather/GamegatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.gamegather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamegatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
